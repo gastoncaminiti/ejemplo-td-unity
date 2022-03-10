d608075f5349ad5ea64f581e64a8e4800e907891
Gaston Caminiti <gastoncaminiti@gmail.com>
2022-03-10 14:18:57 -0300

    Enemy Smooth Movement
    Enemy Prefab
    Refactor FollowPath Coroutine

--- EJEMPLO - TD/Assets/Scripts/EnemyMover.cs
+++ EJEMPLO - TD/Assets/Scripts/EnemyMover.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float delayPath = 1f;
+    [SerializeField][Range(0f, 5f)] float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-       StartCoroutine(FollowPath());
+        StartCoroutine(FollowPath());
     }
 
-    IEnumerator FollowPath(){
+    IEnumerator FollowPath()
+    {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(delayPath);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+            //transform.LookAt(endPosition);
+            while (travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
 }
